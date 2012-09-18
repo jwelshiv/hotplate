71f808bb873b9544ace5aeeb11c74992ab366dbb
jwelshiv <james@supermatter.com>
2012-09-17 19:34:39 -0700

    base rails app and devise install

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Hotplate::Application.routes.draw do
+  devise_for :users
+  
+  devise_scope :user do
+    get "login", :to => "devise/sessions#new"
+    get "logout", :to => "devise/sessions#destroy"
+  end
+
+  root :to => "home#index"
+end
